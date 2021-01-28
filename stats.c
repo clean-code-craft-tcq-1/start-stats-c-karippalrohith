e39bbc0724adbea401c4e6e3c9f1bd397a489de2
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-01-28 23:30:48 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -45,9 +45,7 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 {
     if(computedStats.max > maxThreshold)
     {
-        alerters[0];
-        alerters[1];
-        //emailAlerter();
-        //ledAlerter();
+        emailAlerter();
+        ledAlerter();
     }
 }
